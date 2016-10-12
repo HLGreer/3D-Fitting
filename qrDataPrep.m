d776b1e44047b080ef35db6c4c059d450161f23b
HLGreer <hannahgreer@rogers.com>
2016-10-11 22:03:48 -0400

    Commenting Update
    Updated Comments for better use and understanding.

--- qrDataPrep.m
+++ qrDataPrep.m
@@ -1,10 +1,12 @@
 function [A,b] = qrDataPrep(raw_data, N)
-% Originally produced for Assignment 3 - CISC 271 - Winter 2016
-% Takes the raw data provided in Assignment 3 (CISC 271 - Winter 2016)
-% and produces the A and b for the equation of Ax=b for later QR
-% decoposition and least squares solution.
+% Originally produced for Assignment 3 - CISC 271 - Winter 2016.
+% Takes raw data points as a N by 3 matrix (each row containing the x, y,
+% and z coordinates for a point) and produces the A Matrix and b vector for
+% the equation of Ax=b for later QR decomposition and least squares 
+% solution.
+% Adapted from Class Notes for CISC 271, by Dr. Randy Ellis.
 
-% need to find Ax=b equation, where A is [-2xj^T 1].
+% Need to find Ax=b equation, where A is [-2xj^T 1].
 A = cat(2,zeros(N,3),ones(N,1));
 for j=1:N
     for i=1:3
@@ -12,7 +14,7 @@ for j=1:N
     end
 end
 
-% Now create b for the equation
+% Now create b for the equation.
 b = zeros(N,1);
 for j=1:N
     b(j) = -(raw_data(:,j))'*(raw_data(:,j));
